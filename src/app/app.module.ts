eb85bb5812688442f1516b3c6e6a6b659a413b51
Robin Scholtes <robin@theweirdscience.com>
2020-09-09 16:35:30 +1200

    using hammerjs to detect swipes on mobile devices (swiping right on the mobile menu now closes the menu)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HammerModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -47,6 +48,7 @@ const components = [
   imports: [
     BrowserModule,
     FormsModule,
+    HammerModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
